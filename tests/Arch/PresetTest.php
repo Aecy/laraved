81992738c8ac395d0be0a7ab0fa89b447d11baa8
Aecy <devsnake@hotmail.com>
2025-01-18 18:36:35 +0100

    ci: fixes testing

--- tests/Arch/PresetTest.php
+++ tests/Arch/PresetTest.php
@@ -2,6 +2,19 @@
 
 declare(strict_types=1);
 
-arch()->preset()->php();
-// arch()->preset()->laravel();
-arch()->preset()->security();
+arch('preset PHP')->preset()->php();
+
+arch('strict types')
+    ->expect('App')
+    ->toUseStrictTypes();
+
+arch('preset Laravel')->preset()->laravel()
+    ->ignoring([
+        'App\Providers\Filament\AdminPanelProvider',
+        'App\Filament\Resources\CompetenceResource\Pages\CreateCompetence',
+    ]);
+
+arch('preset Security')
+    ->preset()
+    ->security()
+    ->ignoring('assert');
