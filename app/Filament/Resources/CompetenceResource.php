57923910393ee3c2780a9d6279e1aaabb8212ebf
Aecy <devsnake@hotmail.com>
2025-01-18 18:10:12 +0100

    chore: adds filament for administration

---
+++ app/Filament/Resources/CompetenceResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompetenceResource\Pages;
+use App\Filament\Resources\CompetenceResource\RelationManagers;
+use App\Models\Competence;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CompetenceResource extends Resource
+{
+    protected static ?string $model = Competence::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\FileUpload::make('image')->required(),
+                Forms\Components\TextInput::make('url')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('url'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompetences::route('/'),
+            'create' => Pages\CreateCompetence::route('/create'),
+            'edit' => Pages\EditCompetence::route('/{record}/edit'),
+        ];
+    }
+}
